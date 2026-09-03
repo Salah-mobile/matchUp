87af888a8c36d660d438c2e4b032cddfaa10df02
Salah-mobile <tabitsalah12@gmail.com>
2026-09-03 12:52:36 +0100

    Fix backend repository structure

---
+++ backend/database/migrations/2026_09_03_110633_create_matchs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('matchs', function (Blueprint $table) {
+            $table->id();
+            $table->date("day");
+            $table->time("time");
+            $table->foreignId("place_id")->constrained("places")->onDelete("cascade");
+            $table->foreignId("team1")->constrained("teams")->onDelete('cascade');
+            $table->foreignId("team2")->constrained("teams")->onDelete('cascade');
+            $table->foreignId("winner")->constrained("teams")->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('matchs');
+    }
+};
---
+++ backend/routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use GuzzleHttp\Psr7\Response;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
