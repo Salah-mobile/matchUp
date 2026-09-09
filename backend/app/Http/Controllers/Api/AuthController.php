807d2ce9314b961ffb961f1ad2be5fc09a98d61b
Salah-mobile <tabitsalah12@gmail.com>
2026-09-09 14:01:10 +0100

    create the register function and add the fuction into a route

---
+++ backend/app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request){
+         $request->validate([
+            'name'=>'required|string',
+            'lastname'=>'required|string',
+            'email'=>'required|email',
+            'password'=>'required|min:8'
+         ]);
+         $user=User::create([
+              "name"=>$request->name,
+              "lastname"=>$request->name,
+              "email"=>$request->email,
+              "password"=>$request->password,
+         ]);
+         $token=$user->createToken("auth_tocken")->plainTextToken;
+         return response()->json([
+            "reponse"=>"create user with success",
+            "user"=>$user,
+            "token"=>$token
+         ]);
+    }
+    
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,11 +1,15 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use GuzzleHttp\Psr7\Response;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::controller(AuthController::class)->group(function(){
+     Route::post("/register","register")->name("register");
+     Route::post("/login","login")->name("login");
+     Route::middleware("auth:sanctum")->post("/logout","logout")->name("logout");
+});
+
 
 
